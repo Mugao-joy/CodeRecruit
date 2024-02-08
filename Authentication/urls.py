ea401579f3aa0b918d1a6a2643d6d19238a7493d
Mugao-joy <mugaojoy@gmail.com>
2024-02-08 16:46:43 +0300

    added a path to urls.py of auth of the login and register, created a form for the user to register using inbuilt django forms

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_login
+from .views import user_login, user_register
 
 app_name = 'Authentication'
 urlpatterns = [
-    path ('login/', user_login, name = 'landing')
+    path ('login/', user_login, name = 'landing'),
+    path ('register/', user_register, name = 'register')
 ]
