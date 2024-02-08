34a163468f485727f1fc53c2377b84a93989867a
Mugao-joy <mugaojoy@gmail.com>
2024-02-08 17:05:10 +0300

    in the views.auth we created an if else statement to be able to save details of the user once they input them

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import Userform
 
 def user_register(request):
-    form = Userform()
 
+    if request.method == 'post':
+        form = Userform(request.post)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+
+            return redirect('Authentication:login')
+    else:
+        form = Userform()
     context = {
         'form' : form
     }
