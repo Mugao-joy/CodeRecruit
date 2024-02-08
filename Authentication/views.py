70aca7d0e4593baf3fc85c6cf2bf1d95fbf58128
Mugao-joy <mugaojoy@gmail.com>
2024-02-08 16:40:41 +0300

    added views of login and register of the authentication ; register comes first so that you are redirected to log in once you register

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def user_register(request):
+    return render (request, 'register.html')
+
 # Create your views here.
 def user_login(request):
     return render (request, 'login.html')
