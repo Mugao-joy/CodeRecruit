2195784ca1feb8e7b1f4d5a1671fb943352fc77d
Mugao-joy <mugaojoy@gmail.com>
2024-02-12 11:51:06 +0300

    added static images,carousel,logo

--- job_board/urls.py
+++ job_board/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Authentication.views import user_logout
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,4 @@ urlpatterns = [
     path ('', include ('crjob_board.urls')),
     path('logout/', user_logout, name = 'logout')
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
